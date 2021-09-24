4c17d04dbe06a51648cc8594fdc40a5301849fb0
Saad Essam <77013858+SaadEssam@users.noreply.github.com>
2021-09-24 21:01:16 +0200

    update book from site

--- lms_app/urls.py
+++ lms_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),
-    path('books', views.books, name='books')
+    path('books', views.books, name='books'),
+    path('<int:id>', views.update, name='update'),
 ]
